fd99d3f161493d00049a7ed4540014acc6aa89df
Bibek Kumar Sahoo <43367895+Bibeksahoo9090@users.noreply.github.com>
2018-09-18 13:03:18 +0530

    Online Cart

---
+++ fcart/js/myapp.js
@@ -0,0 +1,37 @@
+var app=angular.module("Myapp",["ngRoute"]);
+	app.config(function($routeProvider){
+	$routeProvider
+	
+	.when("/",{
+		templateUrl:"home.html"
+		
+	})
+	
+	.when("/home",{
+		templateUrl:"home.html"
+		
+	})
+	.when("/about",{
+		templateUrl:"about.html"
+		
+	})
+	.when("/services",{
+		templateUrl:"services.html"
+		
+	})
+	.when("/contact",{
+		templateUrl:"contact.html"
+		
+	})
+	
+	
+});
+
+
+
+
+app.controller("myctrl",function($scope){
+	
+	
+	
+});
